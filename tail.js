235566fd53ff8ac36949c9cfb0f4b171478e81d7
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-04-27 17:38:59 -0400

    added eqArrays

--- tail.js
+++ tail.js
@@ -7,8 +7,8 @@ const assertEqual = function(actual, expected) {
 };
 
 const tail = function(arr) {
-  return arr.slice(1)
-}
+  return arr.slice(1);
+};
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 

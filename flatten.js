f453b4a9c7a475ddc640004b1804fe9643cbd677
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-04-27 22:25:22 -0400

    added flatten

---
+++ flatten.js
@@ -0,0 +1,42 @@
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log('😁😁 Assertion Passed: ' + arr1 + ' === ' + arr2);
+  } else {
+    console.log('😩😩 Assertion Failed: ' + arr1 + ' !== ' + arr2);
+  }
+};
+
+const flatten = function(arr) {
+  
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        newArr.push(arr[i][j]);
+      }
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+
+  return newArr;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]))

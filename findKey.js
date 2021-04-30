ab876998b7a69fd6fe12015985dd6a58b848b6de
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-04-29 22:51:10 -0400

    added findKey

---
+++ findKey.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('😁😁 Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('😩😩 Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+const findKey = function(obj, callback) {
+
+  for (let key in obj) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2))

b9bdaa4c8b8a1f3e2e4cf89c11bfe220c9aedb2d
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-05-03 00:41:55 -0400

    fixed typo

--- eqObjects.js
+++ eqObjects.js
@@ -24,21 +24,18 @@ const eqObjects = function(object1, object2) {
   const obj1Keys = Object.keys(object1);
   const obj2Keys = Object.keys(object2);
   
-  if (obj1Keys.length !== obj2Keys.keys(object2).length) {
+  if (obj1Keys.length !== obj2Keys.length) {
     return false;
 
   } else {
     for (let key of obj1Keys) {
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        if (!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
-      } else if (object1[key] !== object2[key]) {
+      if (object1[key] !== object2[key]) {
         return false;
       }
     }
+    return true;
   }
-  return true;
+  
 };
 
 

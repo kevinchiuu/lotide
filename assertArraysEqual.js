2cee0d0e5179e39952cc6b06d9c7e5c06350b9f9
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-05-03 17:43:34 -0400

    assertArraysEqual refactor

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,5 @@
-const eqArrays = function(arr1, arr2) {
+const eqArrays = require('./eqArrays');
 
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr2[i]) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-};
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
@@ -21,3 +8,5 @@ const assertArraysEqual = function(arr1, arr2) {
     console.log('😩😩 Assertion Failed: ' + arr1 + ' !== ' + arr2);
   }
 };
+
+module.exports = assertArraysEqual;

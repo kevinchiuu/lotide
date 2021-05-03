90285359712ccfb3a4a9ae78f5ba9b166d3f587b
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-05-03 00:33:02 -0400

    edited assertObjectsEqual

---
+++ assertObjectsEqual.js
@@ -0,0 +1,50 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('😁😁 Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('😩😩 Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  const obj1Keys = Object.keys(object1);
+  const obj2Keys = Object.keys(object2);
+  
+  if (obj1Keys.length !== obj2Keys.keys(object2).length) {
+    return false;
+
+  } else {
+    for (let key of obj1Keys) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      } else if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  if (eqObjects(actual, expected)) {
+    console.log('😁😁 Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('😩😩 Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
